2af5b1de03d0bf21ca64161c1c977e5f8fc4aaa3
Spotandjake <40705786+spotandjake@users.noreply.github.com>
2022-02-28 13:37:03 -0500

    Simplify Texture Values

--- RayCastEngine/GameComponents/Types.cs
+++ RayCastEngine/GameComponents/Types.cs
@@ -7,26 +7,26 @@ using System.Threading.Tasks;
 namespace RayCastEngine.GameComponents {
   public enum Texture {
     // Block Textures
-    Air = 0,
-    EagleWall = 1,
-    RedBrickWall = 2,
-    PurpleStoneWall = 3,
-    GreyStoneWall = 4,
-    BlueStoneWall = 5,
-    MossyWall = 6,
-    WoodWall = 7,
-    ColorStoneWall = 8,
+    Air,
+    EagleWall,
+    RedBrickWall,
+    PurpleStoneWall,
+    GreyStoneWall,
+    BlueStoneWall,
+    MossyWall,
+    WoodWall,
+    ColorStoneWall,
     // Entity Texture
-    BarrelEntity = 9,
-    PillarEntity = 10,
-    GreenLight = 11,
+    BarrelEntity,
+    PillarEntity,
+    GreenLight,
     // Enemy Texture
-    Enemy_1 = 12,
+    Enemy_1,
     // Boss Texture
-    Boss_1 = 13,
-    Boss_2 = 14,
-    Boss_3 = 15,
+    Boss_1,
+    Boss_2,
+    Boss_3,
     // Boss Minion Texture
-    Boss_3_Minion_1 = 16
+    Boss_3_Minion_1
   }
 }
